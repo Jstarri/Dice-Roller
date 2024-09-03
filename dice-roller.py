5e1d0443b886ace2b141be1542bbdbba0b13c11b
Ahmar Jenkins <104280394+Jstarri@users.noreply.github.com>
2024-09-02 23:32:25 -0400

    D100 feature

--- dice-roller.py
+++ dice-roller.py
@@ -30,11 +30,16 @@ def d20():
     d_20 = [random.randint(1, 20) for x in range(d_number)]
     print (d_20)
 
+def d100():
+    d_number = int(input("How many dice do you want to roll?"))
+    d_100 = [random.randint(1, 100) for x in range(d_number)]
+    print (d_100)
+
 print("Select your die.")
-print("Die: d4, d6, d8, d10, d12, d20")
+print("Die: d4, d6, d8, d10, d12, d20, d100")
 
 while True:
-    choice = input("Enter choice(d4/d6/d8/d10/d12/d20): ")
+    choice = input("Enter choice(d4/d6/d8/d10/d12/d20/d100): ")
 
     if choice == 'd4':
         d4()
@@ -53,6 +58,9 @@ while True:
       
     elif choice == 'd20':
         d20()
+    
+    elif choice == 'd100':
+        d100()
       
     next_roll = input("Want another roll? (yes/no): ")
     if next_roll == 'no':
