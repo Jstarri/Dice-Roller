0099217a666713a5a8f8265c4efcb17ebcbfead7
Ahmar Jenkins <104280394+Jstarri@users.noreply.github.com>
2025-01-17 06:34:39 -0500

    Text Cleaning

--- dice-roller.py
+++ dice-roller.py
@@ -1,37 +1,37 @@
 import random
 
 def d4():
-    d_number = int(input("How many dice do you want to roll?"))
+    d_number = int(input("How many dice do you want to roll?: "))
     d_4 = [random.randint(1, 4) for x in range(d_number)]
     print (d_4)
 
 def d6():
-    d_number = int(input("How many dice do you want to roll?"))
+    d_number = int(input("How many dice do you want to roll?: "))
     d_6 = [random.randint(1, 6) for x in range(d_number)]
     print (d_6)
 
 def d8():
-    d_number = int(input("How many dice do you want to roll?"))
+    d_number = int(input("How many dice do you want to roll?: "))
     d_8 = [random.randint(1, 8) for x in range(d_number)]
     print (d_8)
 
 def d10():
-    d_number = int(input("How many dice do you want to roll?"))
+    d_number = int(input("How many dice do you want to roll?: "))
     d_10 = [random.randint(1, 10) for x in range(d_number)]
     print (d_10)
 
 def d12():
-    d_number = int(input("How many dice do you want to roll?"))
+    d_number = int(input("How many dice do you want to roll?: "))
     d_12 = [random.randint(1, 12) for x in range(d_number)]
     print (d_12)
 
 def d20():
-    d_number = int(input("How many dice do you want to roll?"))
+    d_number = int(input("How many dice do you want to roll?: "))
     d_20 = [random.randint(1, 20) for x in range(d_number)]
     print (d_20)
 
 def d100():
-    d_number = int(input("How many dice do you want to roll?"))
+    d_number = int(input("How many dice do you want to roll?: "))
     d_100 = [random.randint(1, 100) for x in range(d_number)]
     print (d_100)
 
